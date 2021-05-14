b2754862003dafa50626efc74278e3c048f5a1e9
Conaxian <accounts2@jeniweb.eu>
2021-05-14 18:02:54 +0200

    Improve numeral

--- commands/numeral.py
+++ commands/numeral.py
@@ -14,15 +14,24 @@ import loclib
 async def numeral(ctx):
 
     title = loclib.Loc.member("label_numeral", ctx.author)
+    too_large = False
+
     try:
         number = float(ctx.args["number"])
-        numeral_str = num2words.num2words(number)
-        numeral_str = numeral_str.title().replace("And", "and")
-        embed = cembed.get_cembed(ctx.msg, f"```{numeral_str}```", title)
+        try:
+            numeral_str = num2words.num2words(number)
+            numeral_str = numeral_str.title().replace("And", "and")
+            too_large = len(numeral_str) > 2047
+            embed = cembed.get_cembed(ctx.msg, numeral_str, title)
+        except OverflowError:
+            too_large = True
     except ValueError:
         text = loclib.Loc.member("err_invalid_number", ctx.author)
         embed = cembed.get_cembed(ctx.msg, text)
 
+    if too_large:
+        text = loclib.Loc.member("err_number_too_large", ctx.author)
+        embed = cembed.get_cembed(ctx.msg, text)
     await ctx.channel.send(embed=embed)
 
 ################################################################
