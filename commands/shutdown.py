00cac6e83b090bb49e32650e85935e33bc598680
Conaxian <accounts2@jeniweb.eu>
2021-05-22 13:40:40 +0200

    Improve shutdown

--- commands/shutdown.py
+++ commands/shutdown.py
@@ -14,12 +14,6 @@ async def shutdown(ctx):
     text = loclib.Loc.member("text_exit", ctx.author)
     embed = cembed.get_cembed(ctx.msg, text)
     await ctx.channel.send(embed=embed)
-
-    def crash():
-        try:
-            crash()
-        except:
-            crash()
-    crash()
+    await ctx.client.close()
 
 ################################################################
